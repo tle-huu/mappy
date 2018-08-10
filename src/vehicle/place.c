3dc0cd1a031c51b4dab22e5a4d877fed11d1caf3
spraweczga <spraweczga@o2.pl>
2018-08-09 23:34:14 -0700

    vehicle placing changed to board.data.x/y + 1

--- src/vehicle/place.c
+++ src/vehicle/place.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   place.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nkouris <nkouris@student.42.us.org>        +#+  +:+       +#+        */
+/*   By: psprawka <psprawka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/12 22:58:22 by nkouris           #+#    #+#             */
-/*   Updated: 2018/08/07 14:59:49 by nkouris          ###   ########.fr       */
+/*   Updated: 2018/08/09 23:33:29 by psprawka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,8 @@ static void		goal(t_vehicle *vl)
 	{
 		while (!road)
 		{
-			x = arc4random_uniform((uint32_t)board.data.x);
-			y = arc4random_uniform((uint32_t)board.data.y);
+			x = arc4random_uniform((uint32_t)board.data.x + 1);
+			y = arc4random_uniform((uint32_t)board.data.y + 1);
 			road = (board.data.tiles)[x].column[y].state;
 		}
 		board.data.goal = 1;
@@ -52,8 +52,8 @@ static void		onboard(t_vehicle *vl)
 	road = 0;
 	while (!road)
 	{
-		vl->location.x = arc4random_uniform((uint32_t)board.data.x);
-		vl->location.y = arc4random_uniform((uint32_t)board.data.y);
+		vl->location.x = arc4random_uniform((uint32_t)board.data.x + 1);
+		vl->location.y = arc4random_uniform((uint32_t)board.data.y + 1);
 		vl->location.orientation = 1 << (arc4random_uniform((uint32_t)3));
 		road = (board.data.tiles)[vl->location.x].column[vl->location.y].state;
 	}
@@ -64,16 +64,16 @@ static void		advance(t_vehicle *vl)
 {
 	board.removevehicle(vl);
 	if (vl->location.orientation & NORTH)
-		(vl->location.y == 0) ? vl->location.y = board.data.y :
+		(vl->location.y == 0) ? vl->location.y = board.data.y + 1 :
 			(vl->location.y)--;
 	else if (vl->location.orientation & SOUTH)
-		(vl->location.y == board.data.y) ? vl->location.y = 0 :
+		(vl->location.y == board.data.y + 1) ? vl->location.y = 0 :
 			(vl->location.y)++;
 	else if (vl->location.orientation & WEST)
-		(vl->location.x == 0) ? vl->location.x = board.data.x :
+		(vl->location.x == 0) ? vl->location.x = board.data.x + 1 :
 			(vl->location.x)--;
 	else if (vl->location.orientation & EAST)
-		(vl->location.x == board.data.x) ? vl->location.x = 0 :
+		(vl->location.x == board.data.x + 1) ? vl->location.x = 0 :
 			(vl->location.x)++;
 	board.setvehicle(vl);
 }
